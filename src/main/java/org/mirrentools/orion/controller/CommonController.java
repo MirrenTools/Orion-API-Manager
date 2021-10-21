051a5a57038c918a692adf0e4edd0ff0978284a1
Mirren <690389491@qq.com>
2021-10-21 14:46:53 +0800

    添加单个API导入与导出

--- src/main/java/org/mirrentools/orion/controller/CommonController.java
+++ src/main/java/org/mirrentools/orion/controller/CommonController.java
@@ -5,7 +5,6 @@ import java.util.Map;
 import org.mirrentools.orion.service.CommonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
